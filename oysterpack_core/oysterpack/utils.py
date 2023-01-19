34b14bb74720a01f6351538e493510e30600618c
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2023-01-19 14:46:50 -0500

    updated docs

--- oysterpack_core/oysterpack/utils.py
+++ oysterpack_core/oysterpack/utils.py
@@ -10,14 +10,14 @@ def configure_logging(level: int = logging.WARNING):
     :param level: default = logging.WARNING
     :return: None
 
-    - log format: %(asctime)s %(levelname)s|%(name)s: %(message)s
+    - log format: %(asctime)s [%(levelname)s] [%(name)s] %(message)s
     - timestamps are UTC
     - best practice is to retrieve a logger using the module's name:
 
     >>> configure_logging(level=logging.DEBUG)
     >>> logger = logging.getLogger('oysterpack.algorand')
     >>> logger.info('Algorand is the future of finance') # doctest: +SKIP
-    2023-01-09 14:48:20,594 INFO|oysterpack.algorand: Algorand is the future of finance
+    2023-01-09 14:48:20,594 [INFO] [oysterpack.algorand] Algorand is the future of finance
 
     """
     logging.Formatter.converter = time.gmtime

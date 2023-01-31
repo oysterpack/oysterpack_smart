939688d95192a21c3040625eff3f4b5beada5e92
oysterpack.inc <2zP6OaymGZhJTHFvQMzp6Ze>
2023-01-31 18:48:01 -0500

    removed functions that create and interact with smart contracts because beaker's ApplicationClient already provides the functionality

--- oysterpack/oysterpack/algorand/client/transactions/smart_contract.py
+++ oysterpack/oysterpack/algorand/client/transactions/smart_contract.py
@@ -1,203 +1,24 @@
-import base64
+from base64 import b64encode, b64decode
 
 import algosdk
-from algosdk.transaction import (
-    OnComplete,
-    StateSchema,
-    ApplicationCreateTxn,
-    ApplicationUpdateTxn,
-    ApplicationDeleteTxn,
-    ApplicationOptInTxn,
-    ApplicationCloseOutTxn,
-    ApplicationClearStateTxn,
-    ApplicationNoOpTxn,
-)
-
-from oysterpack.algorand.client.model import Address, AppID, AssetID, BoxKey
-from oysterpack.algorand.client.transactions import GetSuggestedParams, create_lease
 
 
 def base64_encode_arg(arg: bytes | bytearray | str | int) -> bytes:
     """
     Encodes an argument for an application call
     """
-    return base64.b64encode(algosdk.encoding.encode_as_bytes(arg))
+    return b64encode(algosdk.encoding.encode_as_bytes(arg))
 
 
 def base64_decode_int_arg(arg: str | bytes) -> int:
     """
     Decodes an int arg that was encoded for an application call.
     """
-    return int.from_bytes(base64.b64decode(arg))
+    return int.from_bytes(b64decode(arg))
 
 
 def base64_decode_str_arg(arg: str | bytes) -> str:
     """
     Decodes a str arg that was encoded for an application call.
     """
-    return base64.b64decode(arg).decode()
-
-
-def create(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    approval_program: bytes,
-    clear_program: bytes,
-    global_schema: StateSchema,
-    local_schema: StateSchema,
-    app_args: list[bytes] | None = None,
-    accounts: list[Address] | None = None,
-    foreign_apps: list[AppID] | None = None,
-    foreign_assets: list[AssetID] | None = None,
-    note: bytes | None = None,
-    extra_pages: int = 0,
-    boxes: list[tuple[AppID, BoxKey]] | None = None,
-) -> ApplicationCreateTxn:
-    return ApplicationCreateTxn(
-        sender=sender,
-        sp=suggested_params(),
-        lease=create_lease(),
-        on_complete=OnComplete.NoOpOC,
-        approval_program=approval_program,
-        clear_program=clear_program,
-        global_schema=global_schema,
-        local_schema=local_schema,
-        app_args=app_args,
-        accounts=accounts,
-        foreign_apps=foreign_apps,
-        foreign_assets=foreign_assets,
-        extra_pages=extra_pages,
-        note=note,
-        boxes=boxes,
-    )
-
-
-def update(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    app_id: AppID,
-    approval_program: bytes,
-    clear_program: bytes,
-    app_args: list[bytes] | None = None,
-    note: bytes | None = None,
-    boxes: list[tuple[AppID, BoxKey]] | None = None,
-) -> ApplicationUpdateTxn:
-    return ApplicationUpdateTxn(
-        sender=sender,
-        index=app_id,
-        sp=suggested_params(),
-        lease=create_lease(),
-        approval_program=approval_program,
-        clear_program=clear_program,
-        app_args=app_args,
-        note=note,
-        boxes=boxes,
-    )
-
-
-def delete(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    app_id: AppID,
-    note: bytes | None = None,
-) -> ApplicationDeleteTxn:
-    return ApplicationDeleteTxn(
-        sender=sender,
-        index=app_id,
-        sp=suggested_params(),
-        lease=create_lease(),
-        note=note,
-    )
-
-
-def optin(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    app_id: AppID,
-    note: bytes | None = None,
-) -> ApplicationOptInTxn:
-    """
-    An Application Opt-In transaction must be submitted by an account in order for the local state for that account to
-    be used. If no local state is required, this transaction is not necessary for a given account.
-    """
-
-    return ApplicationOptInTxn(
-        sender=sender,
-        index=app_id,
-        sp=suggested_params(),
-        lease=create_lease(),
-        note=note,
-    )
-
-
-def close_out(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    app_id: AppID,
-    note: bytes | None = None,
-) -> ApplicationCloseOutTxn:
-    """
-    An Application Close Out transaction is used when an account wants to opt out of a contract gracefully and remove
-    its local state from its balance record. This transaction may fail according to the logic in the Approval program.
-    """
-
-    return ApplicationCloseOutTxn(
-        sender=sender,
-        index=app_id,
-        sp=suggested_params(),
-        lease=create_lease(),
-        note=note,
-    )
-
-
-def clear_state(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    app_id: AppID,
-    note: bytes | None = None,
-) -> ApplicationClearStateTxn:
-    """
-    An Application Clear State transaction is used to force removal of the local state from the balance record of the
-    sender. Given a well-formed transaction this method will always succeed. The Clear program is used by the application
-    to perform any bookkeeping necessary to remove the Account from its records.
-    """
-
-    return ApplicationClearStateTxn(
-        sender=sender,
-        index=app_id,
-        sp=suggested_params(),
-        lease=create_lease(),
-        note=note,
-    )
-
-
-def call(
-    *,
-    sender: Address,
-    suggested_params: GetSuggestedParams,
-    app_id: AppID,
-    app_args: list[bytes] | None = None,
-    accounts: list[Address] | None = None,
-    foreign_apps: list[AppID] | None = None,
-    foreign_assets: list[AssetID] | None = None,
-    note: bytes | None = None,
-    boxes: list[tuple[AppID, BoxKey]] | None = None,
-) -> ApplicationNoOpTxn:
-    return ApplicationNoOpTxn(
-        sender=sender,
-        sp=suggested_params(),
-        lease=create_lease(),
-        index=app_id,
-        app_args=app_args,
-        accounts=accounts,
-        foreign_apps=foreign_apps,
-        foreign_assets=foreign_assets,
-        note=note,
-        boxes=boxes,
-    )
+    return b64decode(arg).decode()
